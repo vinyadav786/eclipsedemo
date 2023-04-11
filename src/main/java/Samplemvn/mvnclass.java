9164ac6e135de92d39a3ed54e77c013ee519bd32
Vinay Yadav <vinayyadav7892@gmail.com>
2023-04-11 21:50:43 +0530

    initial commit

---
+++ src/main/java/Samplemvn/mvnclass.java
@@ -0,0 +1,10 @@
+package Samplemvn;
+
+public class mvnclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Vinay Yadav");
+	}
+
+}
